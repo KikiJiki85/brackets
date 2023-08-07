ebaff7e471668470293c42bc2f283f2104ee374d
Aleksey Vaskin <alzor85@gmail.com>
2023-08-07 13:36:35 +0300

    feat: finish brackets js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let flag = true;
+  let strTempLength = 0;
+  
+  while(flag) {
+    strTempLength = str.length;
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      while(str.length !== 0) {
+        if (str === str.replace(bracketsConfig[i][0] + bracketsConfig[i][1],'')) break;
+        str = str.replace(bracketsConfig[i][0] + bracketsConfig[i][1],'');
+      }
+    }
+    strTempLength === str.length ? flag = false : null;
+  }
+  
+  return str.length === 0;
 }
